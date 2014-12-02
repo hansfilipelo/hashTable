1ad1268c170eaa81f5ebe97bb799d03f5ed12e06
Hans-Filip Elo <fille@c-205-20.eduroam.liu.se>
2014-12-02 12:35:10 +0100

    Main stuff

--- main.cpp
+++ main.cpp
@@ -18,17 +18,37 @@ int main(){
     table.put("Erhl","Celeron J1900");
     
     string input;
+    bool loop = true;
     
-    cout << "Please enter a name to delete from dictionary" << endl;
-    getline(cin,input);
+    while (loop) {
+        string name;
+        string cpu;
+        
+        cout << "Please enter a name to add dictionary" << endl;
+        getline(cin,name);
+        
+        if (name.size() == 0) {
+            loop = false;
+            break;
+        }
+        cout << "Please enter a CPU for " << name << endl;
+        getline(cin,cpu);
+        
+        table.put(name,cpu);
+    }
     
-    table.remove(input);
+    loop = true;
     
-    cout << "Please enter a name to find out which CPU they're using" << endl;
-    getline(cin,input);
-    
-    cout << table.get(input) << endl;
-    
-    
-    
+    while (loop) {
+        
+        
+        cout << "Please enter a name to find out which CPU they're using" << endl;
+        getline(cin,input);
+        
+        if (input.size() == 0) {
+            loop = false;
+            break;
+        }
+        cout << table.get(input) << endl;
+    }
 }
